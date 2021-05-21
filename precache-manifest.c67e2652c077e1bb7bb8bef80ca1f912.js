344628fa55f84c46903efd96177c74d45679121c
Shuo Han <shuolhan@gmail.com>
2021-05-20 22:30:27 -0700

    Updates

---
+++ precache-manifest.c67e2652c077e1bb7bb8bef80ca1f912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a480197d56b4121b4cdc98f7eb88a9",
+    "url": "/Metabolomics_Data_Explorer/index.html"
+  },
+  {
+    "revision": "749383fd213de79d7245",
+    "url": "/Metabolomics_Data_Explorer/static/css/main.aa99a0b8.chunk.css"
+  },
+  {
+    "revision": "b3ce6fb90428e4d01d2c",
+    "url": "/Metabolomics_Data_Explorer/static/js/2.98fe61f5.chunk.js"
+  },
+  {
+    "revision": "749383fd213de79d7245",
+    "url": "/Metabolomics_Data_Explorer/static/js/main.7d8e3cd6.chunk.js"
+  },
+  {
+    "revision": "2f57eef6bac610b27eb4",
+    "url": "/Metabolomics_Data_Explorer/static/js/runtime~main.97cd71b4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Metabolomics_Data_Explorer/precache-manifest.c67e2652c077e1bb7bb8bef80ca1f912.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Metabolomics_Data_Explorer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
